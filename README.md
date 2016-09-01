296c4e7f842d1203929f85f5798c4d925b46ebcf
gkar68 <gkar68@yahoo.com>
2016-08-31 22:38:29 -0600

    add part_scanner

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SecurityTools
 Tools for security using Python3.5
+
+## Scripts ##
+
+This is the list of the tools:
+
+#### port_scanner.py ####
+The script will ask you about the host name. It will resolve it to IP4 address, and then scan for which ports are open and display the status.
---
+++ port_scanner.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.5
+import socket
+import subprocess
+import sys
+from datetime import datetime
+
+remoteServer = input("Enter a remote host to scan (example google.com): ")
+remoteServerIP = socket.gethostbyname(remoteServer)
+
+print ("IP4 address for the server is:" , remoteServerIP)
+
+try:
+    for port in range(1, 1025):
+        print("testing port {}: ".format(port), end="", flush=True)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result == 0:
+            print("is open")
+        else:
+            print("is close")
+        sock.close()
+
+except socket.gaierror:
+    print ("Hostname could not be resolved")
+    sys.exit()
+
+except socket.error:
+    print("Cannot connect to the server")
+    sys.exit()
